996d2280ffa585f704d22ac3e2783a72d4d10179
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2020-02-13 11:46:59 +0100

    Add models and routes for creating and updating and fetching users

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express')
 
 const app = express()
+const connectDB = require('./config/db')
 
-const PORT = process.env.PORT || 5000
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const auth = require('./routes/api/auth')
+const posts = require('./routes/api/posts')
+
+app.use(express.json({ extended: false }))
+
+connectDB()
 
 app.get('/', (req, res) => res.send('API running!'))
 
-app.listen(PORT, () => console.log(`Server is up and running at: ${PORT}`))
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/auth', auth)
+app.use('/api/posts', posts)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () =>
+  console.log(`
+***
+Server is up and running at: ${PORT}
+***
+`)
+)
