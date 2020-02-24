19d1be9b4f029bf536852040a544e66ea8ca5484
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2020-02-24 11:43:52 +0100

    Add commentfunctions

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PostItem from '../posts/PostItem';
+import CommentForm from '../post/CommentForm';
+import CommentItem from '../post/CommentItem';
 import Spinner from '../layout/Spinner';
 import { getPost } from '../../actions/post';
 
@@ -19,6 +21,12 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         Back to Posts!
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map(comment => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
